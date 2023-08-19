c7cd2bf1a41d411875152006aa14b358093a808e
Jayaveerasabari Velusamy <jayaveerasabarivelusamy@gmail.com>
2023-08-19 15:34:45 +0530

    Updated the message for all Get

---
+++ ListAllRoom.js
@@ -0,0 +1,34 @@
+import { MongoClient } from "mongodb";
+
+import express from "express";
+
+const app = express();
+
+const mongourl = "mongodb://127.0.0.1:27017";
+// Mongo DB
+async function createConnection() {
+  const client = new MongoClient(mongourl);
+  await client.connect();
+  console.log("Mongo DB is Connected");
+  return client;
+}
+
+const client = await createConnection();
+
+app.get("/GetAllRoomDetails", async function (req, res) {
+  const { RoomNo } = req.params;
+
+  console.log(req.params, RoomNo);
+  const book = await client
+    .db("NodeJSAPI")
+    .collection("ListAllRoom")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  res.send(book);
+});
+
+app.listen(4000);
